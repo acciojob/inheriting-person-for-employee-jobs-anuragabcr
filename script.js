7cd1150170939951390e59a0287c6fc09786f60d
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-20 16:49:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // complete this js code
-class Person() {
+class Person {
 	constructor(name, age) {
 		this.name = name
 		this.age = age
@@ -9,7 +9,7 @@ class Person() {
 	}
 }
 
-class Employee() extends Person {
+class Employee extends Person {
 	constructor(name, age, jobTitle) {
 		super(name, age)
 		this.jobTitle = jobTitle
